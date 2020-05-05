b173bfea2b537c15a7a00e3f0713c8b785a2e7e5
karancsi <zsofaa9877@citromail.hu>
2020-05-05 22:55:23 +0200

    static data to html

--- ViewEmail/src/main/java/NewsLetterEmail/Controller/EmailController.java
+++ ViewEmail/src/main/java/NewsLetterEmail/Controller/EmailController.java
@@ -34,6 +34,7 @@ import lombok.extern.slf4j.Slf4j;
 public class EmailController {
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(EmailController.class);
 
+	
 	private EmailLogic ELogic;
 	private EmailDto ed;
 	private NewsLetterSer ser;
